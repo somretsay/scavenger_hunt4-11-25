6b1dd6f165070b8cbb653acc20d02c49489e6460
somretsay <112686697+somretsay@users.noreply.github.com>
2025-04-14 17:18:09 -1000

    Add files via upload

---
+++ scavenger_hunt/hunt/routing.py
@@ -0,0 +1,10 @@
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/team/(?P<team_id>\w+)/$', consumers.TeamConsumer.as_asgi()),
+    re_path(r'ws/lobby/(?P<lobby_id>\w+)/$', consumers.LobbyConsumer.as_asgi()),
+    re_path(r'ws/available-teams/$', consumers.AvailableTeamsConsumer.as_asgi()),
+    re_path(r'ws/race/(?P<race_id>\w+)/$', consumers.RaceConsumer.as_asgi()),
+    re_path(r'ws/leaderboard/$', consumers.LeaderboardConsumer.as_asgi()),
+]
